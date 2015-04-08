868520f6c0e4f137efc34af5b90e0143ed5f4a19
SMALL-TIGER <15501253762@163.com>
2015-04-08 15:34:30 +0800

    链表

---
+++ leetcode/src/easy/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class RemoveDuplicatesfromSortedList {
+	public ListNode deleteDuplicates(ListNode head) {
+		if(head==null){
+			return null;
+		}
+		ListNode q = head;
+		ListNode p = head.next;
+		while (p != null) {
+			if (p.val == q.val) {
+				q.next = p.next;
+			} else {
+				q = p;
+			}
+			p = p.next;
+		}
+		return head;
+	}
+}
