d319b2bf2cbcb841970ee4437b5a2ad2dbc7aafb
SMALL-TIGER <15501253762@163.com>
2015-04-08 18:11:59 +0800

    求树深度，层序遍历

---
+++ leetcode/src/easy/MaximumDepthofBinaryTree.java
@@ -0,0 +1,33 @@
+package easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepthofBinaryTree {
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int cnt1 = 1, cnt2 = 0, level = 0;
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			TreeNode t = queue.poll();
+			cnt1--;
+			if (t.left != null) {
+				queue.offer(t.left);
+				cnt2++;
+			}
+			if (t.right != null) {
+				queue.offer(t.right);
+				cnt2++;
+			}
+			if (cnt1 == 0) {
+				cnt1 = cnt2;
+				cnt2 = 0;
+				level++;
+			}
+		}
+		return level;
+	}
+}
