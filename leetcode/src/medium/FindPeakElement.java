c3d35c660c853074067da96bc0522e56372c9898
SMALL-TIGER <15501253762@163.com>
2015-05-14 16:58:20 +0800

    二分查找峰值

--- leetcode/src/medium/FindPeakElement.java
+++ leetcode/src/medium/FindPeakElement.java
@@ -1,6 +1,9 @@
 package medium;
 
 public class FindPeakElement {
+	/**
+	 * 顺序查找，时间复杂度O(N) 
+	 */
     public int findPeakElement(int[] nums) {
     	if(nums.length==1 || nums[0]>nums[1]){
     		return 0;
@@ -14,5 +17,23 @@ public class FindPeakElement {
 			}
 		}
     	return 0;
+    }
+    /**
+     * 二分查找：时间复杂度O(logN)
+     * 如果某个数mid大于其右邻，那么一定有峰值在mid左侧，否则有峰值在右侧
+     */
+    public int findPeakElement_binary(int[] nums) {
+    	int left=0;
+    	int right=nums.length-1;
+    	while(left<right){
+    		int mid=(left+right)/2;
+    		if(nums[mid]<nums[mid+1]){
+    			left=mid+1;
+    		}else{
+    			//注意不能用mid-1，因为可能是mid
+    			right=mid;
+    		}
+    	}
+    	return left;
     }
 }
