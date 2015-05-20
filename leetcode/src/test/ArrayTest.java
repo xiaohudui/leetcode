b3172edb59f6d43017e179a78838a326f532a59e
SMALL-TIGER <15501253762@163.com>
2015-05-20 15:18:01 +0800

    增加测试

--- leetcode/src/test/ArrayTest.java
+++ leetcode/src/test/ArrayTest.java
@@ -1,8 +1,10 @@
 package test;
 
+import medium.HouseRobberII;
+
 public class ArrayTest {
 	public static void main(String[] args) {
 		int[] nums={4,5,6,7,0,1,2};
-		
+		System.out.println(new HouseRobberII().rob(nums));
 	}
 }
--- leetcode/src/test/LinkedListTest.java
+++ leetcode/src/test/LinkedListTest.java
@@ -1,20 +1,26 @@
 package test;
 
-import medium.ReorderList;
+import medium.InsertionSortList;
 import util.ListNode;
 
 public class LinkedListTest {
 	public static void main(String[] args) {
-		ListNode one=new ListNode(1);
-		ListNode two=new ListNode(2);
-		ListNode three=new ListNode(3);
-		ListNode four=new ListNode(4);
-		ListNode five=new ListNode(5);
-		one.next=two;
-		two.next=three;
-		three.next=four;
-		four.next=five;
-		five.next=null;
-		new ReorderList().reorderList(one);
+		ListNode head = new ListNode(3);
+		ListNode one = new ListNode(4);
+		ListNode two = new ListNode(1);
+		ListNode three = new ListNode(3);
+		ListNode four = new ListNode(4);
+		ListNode five = new ListNode(2);
+		head.next = one;
+		one.next = two;
+		two.next = null;
+//		three.next = four;
+//		four.next = five;
+//		five.next = null;
+		ListNode res = new InsertionSortList().insertionSortList(head);
+		while (res != null) {
+			System.out.println(res.val);
+			res=res.next;
+		}
 	}
 }
