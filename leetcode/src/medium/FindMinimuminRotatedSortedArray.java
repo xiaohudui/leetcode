33f749554361f13afaed79c2756e58e86a564274
SMALL-TIGER <15501253762@163.com>
2015-05-20 11:01:06 +0800

    二分查找

--- leetcode/src/medium/FindMinimuminRotatedSortedArray.java
+++ leetcode/src/medium/FindMinimuminRotatedSortedArray.java
@@ -25,8 +25,35 @@ public class FindMinimuminRotatedSortedArray {
     	}
     	return nums[l];
     }
+    /**
+     * 根据左右半边分别递增，如果左边大于中间值，那么最小值一定在左边和中间值之间
+     * 否则，左边元素就是左半边的最小值，然后去找右半边的最小值
+     */
+    public int findMin1(int[] nums) {
+    	if(nums==null || nums.length==0){
+    		return 0;
+    	}
+    	int l=0;
+    	int r=nums.length-1;
+    	int m=(l+r)/2;
+    	int min=nums[0];
+    	while(l<=r){
+    		if(nums[l]<nums[m]){
+    			min=Math.min(min, nums[l]);
+    			l=m+1;
+    		}else if(nums[l]>nums[m]){
+    			min=Math.min(min, nums[m]);
+    			r=m-1;
+    		}else{
+    			min=Math.min(min, Math.min(nums[l], nums[r]));
+    			break;
+    		}
+    		m=(l+r)/2;
+    	}
+    	return min;
+    }
     public static void main(String[] args) {
-    	int[] A={4,5,6,7,0,1,2};
-    	System.out.println(new FindMinimuminRotatedSortedArray().findMin(A));
+    	int[] A={2,3,1};
+    	System.out.println(new FindMinimuminRotatedSortedArray().findMin1(A));
 	}
 }
