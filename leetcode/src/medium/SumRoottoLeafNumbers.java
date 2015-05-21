581097fb31aa23304050d5f36d51d8ef4aae523a
SMALL-TIGER <15501253762@163.com>
2015-05-21 09:27:32 +0800

    树的道路和 递归

---
+++ leetcode/src/medium/SumRoottoLeafNumbers.java
@@ -0,0 +1,19 @@
+package medium;
+
+import util.TreeNode;
+
+public class SumRoottoLeafNumbers {
+	public int sumNumbers(TreeNode root) {
+		return sum(root, 0);
+	}
+	public int sum(TreeNode root, int sum) {
+		if (root == null) {
+			return 0;
+		}
+		sum = sum * 10 + root.val;
+		if(root.left==null && root.right==null){
+			return sum;
+		}
+		return sum(root.left, sum) + sum(root.right, sum);
+	}
+}
--- leetcode/src/test/TreeTest.java
+++ leetcode/src/test/TreeTest.java
@@ -1,20 +1,19 @@
 package test;
 
-import medium.InOrder;
+import medium.SumRoottoLeafNumbers;
 import util.TreeNode;
 
 public class TreeTest {
 	public static void main(String[] args) {
-		TreeNode t1 = new TreeNode(10);
-		TreeNode t2 = new TreeNode(5);
-		TreeNode t3 = new TreeNode(15);
-		TreeNode t4 = new TreeNode(6);
-		TreeNode t5 = new TreeNode(20);
+		TreeNode t1 = new TreeNode(1);
+		TreeNode t2 = new TreeNode(2);
+		TreeNode t3 = new TreeNode(3);
+		TreeNode t4 = new TreeNode(4);
+		TreeNode t5 = new TreeNode(5);
 		t1.left = t2;
 		t1.right = t3;
 		t3.left = t4;
 		t3.right = t5;
-		InOrder it=new InOrder();
-		System.out.println(it.traverse(t1));
+		System.out.println(new SumRoottoLeafNumbers().sumNumbers(t1));
 	}
 }
