866de536c44c71e41fa470fada34daeceeb4a082
xiaohudui <m15501253762@163.com>
2015-06-24 14:19:46 +0800

    二分

--- leetcode/src/medium/SearchInsertPosition.java
+++ leetcode/src/medium/SearchInsertPosition.java
@@ -15,7 +15,7 @@ public class SearchInsertPosition {
     	return i;
     }
     /**
-     * 二分查找
+     * 二分查找到大概位置，然后分别处理
      * 注：二分查找正常是在元素存在的假设下
      */
     public int binarySearchInsert(int[] nums,int target){
@@ -38,5 +38,23 @@ public class SearchInsertPosition {
     	}else{
     		return low;
     	}
+    }
+    /**
+     * 不用最后判断的二分查找
+     */
+    public int binarySearchInsert1(int[] nums,int target){
+    	int high=nums.length-1;
+    	int low=0;
+    	while(high>=low){
+    		int mid=(high+low)/2;
+    		if(nums[mid]>target){
+    			high=mid-1;
+    		}else if(nums[mid]<target){
+    			low=mid+1;
+    		}else{
+    			return mid;
+    		}
+    	}
+    	return low;
     }
 }
