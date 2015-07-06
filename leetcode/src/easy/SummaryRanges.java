4d3ed70d6619d2c6b375a973304add615c00a961
xiaohudui <m15501253762@163.com>
2015-07-06 11:16:16 +0800

    细节，考虑空，一个，开始，结束

--- leetcode/src/easy/SummaryRanges.java
+++ leetcode/src/easy/SummaryRanges.java
@@ -7,8 +7,6 @@ public class SummaryRanges {
 	/**
 	 * 考察思考的严密性，注意第一个，最后一个，唯一的一个，一个也没有等情况。
 	 * 区别对待最后一个，
-	 * @param nums
-	 * @return
 	 */
 	public List<String> summaryRanges(int[] nums) {
 		List<String> res = new ArrayList<String>();
@@ -43,8 +41,31 @@ public class SummaryRanges {
 		}
 		return res;
 	}
+	/**
+	 * 简洁算法
+	 */
+	public List<String> summaryRanges1(int[] nums) {
+		List<String> res = new ArrayList<String>();
+		if (nums == null || nums.length == 0) {
+			return res;
+		}
+		int i=0,j=1;
+		while(i<nums.length){
+			j=1;
+			while(i+j<nums.length && nums[i+j]-nums[i]==j){
+				j++;
+			}
+			if(j==1){
+				res.add(String.valueOf(nums[i]));
+			}else{
+				res.add(String.valueOf(nums[i])+"->"+String.valueOf(nums[i+j-1]));
+			}
+			i+=j;
+		}
+		return res;
+	}
 	public static void main(String[] args) {
-		int[] nums = {1,2,4};
-		System.out.println(new SummaryRanges().summaryRanges(nums));
+		int[] nums = {1,2};
+		System.out.println(new SummaryRanges().summaryRanges1(nums));
 	}
 }
