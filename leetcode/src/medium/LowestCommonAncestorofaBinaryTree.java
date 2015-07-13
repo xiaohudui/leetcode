b7b443f6ba8c1156f3374f500cb92792e8c6d1f7
xiaohudui <m15501253762@163.com>
2015-07-13 15:31:04 +0800

    树的递归

---
+++ leetcode/src/medium/LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,23 @@
+package medium;
+
+import util.TreeNode;
+
+public class LowestCommonAncestorofaBinaryTree {
+	/**
+	 * 树的递归 
+	 */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    	if(root==null){
+    		return null;
+    	}
+    	if(root==p || root==q){
+    		return root;
+    	}
+    	TreeNode left=lowestCommonAncestor(root.left, p, q);
+    	TreeNode right=lowestCommonAncestor(root.right, p, q);
+    	if(left!=null && right!=null){
+    		return root;
+    	}
+    	return left!=null?left:right;
+    }
+}
