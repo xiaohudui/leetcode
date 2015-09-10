d06c5a4746c7836cd1fc787dce92006c79e74b18
xiaohudui <m15501253762@163.com>
2015-09-10 16:14:49 +0800

    数组指针移动

--- leetcode/src/medium/RemoveDuplicatesfromSortedArrayII.java
+++ leetcode/src/medium/RemoveDuplicatesfromSortedArrayII.java
@@ -3,24 +3,21 @@ package medium;
 import java.util.Arrays;
 
 public class RemoveDuplicatesfromSortedArrayII {
-    public int removeDuplicates(int[] nums) {
-    	if(nums==null ||nums.length==0){
-    		return 0;
-    	}
-    	if(nums.length==1){
-    		return 1;
-    	}
-        int p=2;
-        for (int i = 2; i < nums.length; i++) {
-			if(!(nums[i]==nums[i-1] && nums[i]==nums[i-2])){
-				nums[p]=nums[i];
+	public int removeDuplicates(int[] nums) {
+		if (nums.length <= 2){
+			return nums.length;
+		}
+		int p=1;
+		for (int i = 2; i < nums.length; i++) {
+			if(!(nums[i]==nums[p] && nums[i]==nums[p-1])){
 				p++;
+				nums[p]=nums[i];
 			}
 		}
-        return p;
-    }
-    public static void main(String[] args) {
-		int[] nums={1,2,2,2,2,2,3,4,4,4};
+		return p+1;
+	}
+	public static void main(String[] args) {
+		int[] nums = {1, 1, 1, 2, 2, 3};
 		System.out.println(new RemoveDuplicatesfromSortedArrayII().removeDuplicates(nums));
 		System.out.println(Arrays.toString(nums));
 	}
