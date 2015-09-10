071a2a2dc87a9d8a54dc1bcc539cc6c669015f18
xiaohudui <m15501253762@163.com>
2015-09-10 17:08:16 +0800

    链表

--- leetcode/src/medium/RemoveDuplicatesfromSortedListII.java
+++ leetcode/src/medium/RemoveDuplicatesfromSortedListII.java
@@ -1,34 +1,22 @@
 package medium;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import util.ListNode;
 
 public class RemoveDuplicatesfromSortedListII {
-    public ListNode deleteDuplicates(ListNode head) {
-    	Map<Integer,ListNode> map=new HashMap<Integer,ListNode>();
-    	List<ListNode> removed=new ArrayList<ListNode>();
-    	ListNode newHead=new ListNode(0);
-    	newHead.next=head;
-    	ListNode p=newHead;
-    	ListNode q=newHead.next;
-    	while(q!=null){
-    		if(map.containsKey(q.val)){
-    			p.next=q.next;
-    			removed.add(map.get(q.val));
-    		}else{
-    			map.put(q.val, p);
-    			p=p.next;
-    		}
-    		q=p.next;
-    	}
-    	for(int i=removed.size()-1;i>=0;i--){
-    		ListNode node=removed.get(i);
-    		node.next=node.next.next;
-    	}
-    	return newHead.next;
-    }
+	public ListNode deleteDuplicates(ListNode head) {
+		ListNode newHead = new ListNode(0);
+		newHead.next = head;
+		ListNode p = newHead;
+		while (p.next != null && p.next.next != null) {
+			if (p.next.val == p.next.next.val) {
+				int temp = p.next.val;
+				while (p.next!=null && p.next.val == temp) {
+					p.next = p.next.next;
+				}
+			} else {
+				p = p.next;
+			}
+		}
+		return newHead.next;
+	}
 }
--- leetcode/src/test/LinkedListTest.java
+++ leetcode/src/test/LinkedListTest.java
@@ -9,14 +9,14 @@ public class LinkedListTest {
 	public static void main(String[] args) {
 		ListNode head = new ListNode(1);
 		ListNode one = new ListNode(1);
-		ListNode two = new ListNode(2);
+		ListNode two = new ListNode(1);
 		ListNode three = new ListNode(2);
-		ListNode four = new ListNode(1);
-		ListNode five = new ListNode(1);
+		ListNode four = new ListNode(2);
+		ListNode five = new ListNode(2);
 		head.next = one;
 		one.next = two;
 		two.next = three;
-		three.next = null;
+		three.next = four;
 		four.next = five;
 		five.next = null;
 		ListNode res = new RemoveDuplicatesfromSortedListII().deleteDuplicates(head);
