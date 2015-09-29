a8315e028fd555ebc6f4905ae4f79f66c2410803
xiaohudui <m15501253762@163.com>
2015-09-29 11:39:19 +0800

    添加图片

--- leetcode/src/medium/ThreeSum.java
+++ leetcode/src/medium/ThreeSum.java
@@ -10,46 +10,46 @@ public class ThreeSum {
 	/**
 	 * 思路二：选定target时，不选重复的，因为和该target配套的twosum对都已经找到了，找twosum对时也略过重复的
 	 */
-	public ArrayList<ArrayList<Integer>> threeSum1(int[] num){
+	public ArrayList<ArrayList<Integer>> threeSum1(int[] nums){
 	    ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-	    if(num==null || num.length<=2){
+	    if(nums==null || nums.length<=2){
 	        return res;
 	    }
-	    Arrays.sort(num);
-	    for(int i=num.length-1;i>=2;i--){
-	        if(i<num.length-1 && num[i]==num[i+1]){
+	    Arrays.sort(nums);
+	    for(int i=nums.length-1;i>=2;i--){
+	        if(i<nums.length-1 && nums[i]==nums[i+1]){
 	        	continue;
 	        }
-	         ArrayList<ArrayList<Integer>> curRes = twoSum(num,i-1,-num[i]);
+	         ArrayList<ArrayList<Integer>> curRes = twoSum(nums,i-1,-nums[i]);
 	         for(int j=0;j<curRes.size();j++){
-	             curRes.get(j).add(num[i]);
+	             curRes.get(j).add(nums[i]);
 	         }
 	         res.addAll(curRes);
 	    }
 	    return res;
 	}
-	private ArrayList<ArrayList<Integer>> twoSum(int[] num, int end,int target){
+	private ArrayList<ArrayList<Integer>> twoSum(int[] nums, int end,int target){
 	    ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-	    if(num==null || num.length<=1)
+	    if(nums==null || nums.length<=1)
 	        return res;
 	    int l = 0;
 	    int r = end;
 	    while(l<r){
-	        if(num[l]+num[r]==target){
+	        if(nums[l]+nums[r]==target){
 	            ArrayList<Integer> item = new ArrayList<Integer>();
-	            item.add(num[l]);
-	            item.add(num[r]);
+	            item.add(nums[l]);
+	            item.add(nums[r]);
 	            res.add(item);
 	            l++;
 	            r--;
-	            while(l<r&&num[l]==num[l-1]){
+	            while(l<r&&nums[l]==nums[l-1]){
 	                l++;
 	            }
-	            while(l<r&&num[r]==num[r+1]){
+	            while(l<r&&nums[r]==nums[r+1]){
 	                r--;
 	            }
 	        }
-	        else if(num[l]+num[r]>target){
+	        else if(nums[l]+nums[r]>target){
 	            r--;
 	        }
 	        else{
@@ -60,16 +60,16 @@ public class ThreeSum {
 	}
 	/**
 	 * 思路一：转化为twosum，利用set去重
-	 * @param num
+	 * @param nums
 	 * @return
 	 */
-	public List<List<Integer>> threeSum(int[] num) {
+	public List<List<Integer>> threeSum(int[] nums) {
 		HashSet<Triple> set = new HashSet<Triple>();
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
-		Arrays.sort(num);
-		for (int i = 0; i < num.length; i++) {
-			int target = 0 - num[i];
-			twoSum(set, target, num,i);
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length; i++) {
+			int target = 0 - nums[i];
+			twoSum(set, target, nums,i);
 		}
 		List<Integer> list;
 		for (Triple t : set) {
@@ -81,15 +81,15 @@ public class ThreeSum {
 		}
 		return res;
 	}
-	void twoSum(Set<Triple> set, int target, int[] num,int start) {
-		int i=start+1,j=num.length-1;
+	void twoSum(Set<Triple> set, int target, int[] nums,int start) {
+		int i=start+1,j=nums.length-1;
 		while(i<j){
-			if(num[i]+num[j]>target){
+			if(nums[i]+nums[j]>target){
 				j--;
-			}else if(num[i]+num[j]<target){
+			}else if(nums[i]+nums[j]<target){
 				i++;
 			}else{
-				set.add(new Triple(-target,num[i],num[j]));
+				set.add(new Triple(-target,nums[i],nums[j]));
 				i++;
 				j--;
 			}
